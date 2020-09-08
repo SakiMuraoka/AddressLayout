9cd4b738b89fa3b759d919999d9758b765cc76df
SakiMuraoka <murasaki9322@gmail.com>
2020-09-08 12:33:12 +0900

    create project

---
+++ AddressLayout/AddressLayout/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AddressLayout
+//
+//  Created by 村岡沙紀 on 2020/09/08.
+//  Copyright © 2020 村岡沙紀. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
