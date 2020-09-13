e57f76c937bb233c30147de332a6300bfd741df1
SakiMuraoka <murasaki9322@gmail.com>
2020-09-13 20:34:26 +0900

    フィルター検索をできるようにした，@ObservedObjectのfilterDataでlogView，filterView，itemList間で値を利用

--- AddressLayout/AddressLayout/itemList.swift
+++ AddressLayout/AddressLayout/itemList.swift
@@ -9,22 +9,94 @@
 import SwiftUI
 
 struct itemList: View {
-    var searchText: String
+    @ObservedObject var filterData: ViewModel
+    
     var body: some View {
         List {
             ForEach(testLogData) {LogData in
-                if(self.searchText == "" || (LogData.sender.address.prefecture + LogData.sender.address.city + LogData.sender.address.region).contains(self.searchText) || (LogData.receiver.address.prefecture + LogData.receiver.address.city + LogData.receiver.address.region).contains(self.searchText) || (LogData.sender.name).contains(self.searchText) || (LogData.receiver.name).contains(self.searchText)){
-                    NavigationLink(destination: PreviewView(logData: LogData)) {itemRow(LogData: LogData)
+                if(filter(text: self.filterData.searchText, LogData: LogData, filterData: self.filterData)){
+                    NavigationLink(destination: PreviewView(logData: LogData)) {
+                        itemRow(LogData: LogData)
+                    }
+                }
+            }
+        }
+       
+    }
+}
+
+func filter(text: String, LogData: Log, filterData: ViewModel) -> Bool{
+    var isHidden = true
+    let senderAddress = LogData.sender.address.prefecture + LogData.sender.address.city + LogData.sender.address.region
+    let receiverAddress = LogData.receiver.address.prefecture + LogData.receiver.address.city + LogData.receiver.address.region
+    let senderNames = (LogData.sender.name).components(separatedBy: ",")
+    let receiverNames = (LogData.receiver.name).components(separatedBy: ",")
+    var senderName = ""
+    var receiverName = ""
+    for name in senderNames{
+        senderName += name
+    }
+    for name in receiverNames{
+        receiverName += name
+    }
+    if(text != "" && !senderAddress.contains(text) && !receiverAddress.contains(text) && !senderName.contains(text) && !receiverName.contains(text)){
+        isHidden.toggle()
+    }else{
+        if(filterData.isOrganization){
+            if(filterData.lastname != "" && filterData.lastname != receiverNames[0]){
+                isHidden.toggle()
+            }else if(filterData.firstname != "" && filterData.firstname != receiverNames[1]){
+                isHidden.toggle()
+            }
+        }else{
+            if(filterData.name != "" && filterData.name != receiverName){
+                isHidden.toggle()
+            }
+        }
+        if(isHidden){
+            if(filterData.zipcode != "" && Int(filterData.zipcode) != LogData.receiver.address.zipcode){
+                isHidden.toggle()
+            }else{
+                if(filterData.prefecture != "" && filterData.prefecture != LogData.receiver.address.prefecture){
+                    isHidden.toggle()
+                }else{
+                    if(filterData.city != "" && filterData.city != LogData.receiver.address.city){
+                        isHidden.toggle()
+                    }else{
+                        if(filterData.region != "" && filterData.region != LogData.receiver.address.region){
+                            isHidden.toggle()
+                        }else{
+                            if(filterData.number1str != "" && Int(filterData.number1str) != LogData.receiver.address.number1){
+                                isHidden.toggle()
+                            }else{
+                                if(filterData.number2str != "" && Int(filterData.number2str) != LogData.receiver.address.number2){
+                                    isHidden.toggle()
+                                }else{
+                                    if(filterData.number3str != "" && Int(filterData.number3str) != LogData.receiver.address.number3){
+                                        isHidden.toggle()
+                                    }else{
+                                        if(filterData.building != "" && filterData.building != LogData.receiver.address.building){
+                                            isHidden.toggle()
+                                        }else{
+                                            if(filterData.roomNumberstr != "" && Int(filterData.roomNumberstr) != LogData.receiver.address.roomNumber){
+                                            isHidden.toggle()
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
                     }
                 }
             }
         }
     }
+    return isHidden
 }
 
 struct itemList_Previews: PreviewProvider {
     static var previews: some View {
-        itemList(searchText: "")
+        itemList(filterData: ViewModel())
     }
 }
 
